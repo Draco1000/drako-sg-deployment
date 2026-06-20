f9e88ced220e7c8f55636765a187a94bf719142c
Daniel GOTECpro <105630984+Draco1000@users.noreply.github.com>
2026-06-20 17:18:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "drako_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.drako_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.drako_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  cidr_ipv6         = data.aws_vpc.main.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -34,4 +34,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.drako_tls.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+}
+data "aws_vpc" "main" {
+  id = var.vpc_id
 }
