165bcad56be95f9bab12ef8acb562b65d1391926
Daniel GOTECpro <105630984+Draco1000@users.noreply.github.com>
2026-06-20 16:34:20 +0200

    Update AWS provider version constraint

--- provider.tf
+++ provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.49.0"
+      version = "~> 6.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = var.region
 }
